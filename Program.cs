fc57c391e528753d528e7554b58e7320957e3412
Tess1407 <132476402+Tess1407@users.noreply.github.com>
2023-07-07 19:37:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int lenght = 12;
+        Console.WriteLine(randPassword(lenght));
+    }
+    static char[] randPassword(int lenght)
+    {
+        Console.WriteLine("Generating password using random");
+        Console.Write("Your new password will be: ");
+        string CapitalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string SmallChars = "abcdefghijklmnopqrstuvwxyz";
+        string numbers = "0123456789";
+        string symbols = "!?*+-";
+        string values = CapitalChars + SmallChars + numbers + symbols;
+        Random rand = new Random();
+        char[] password = new char[lenght];
+        for (int i =0; i  < lenght; i++)
+        {
+            password[i] = values[(rand.Next(values.Length))];
+        }
+        return password;
+    }
+}
